87ed61b94b5920a6da1bb68fe00a90b41050e06e
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2023-12-28 11:42:04 +0300

    searching

--- src/ca2110/searching.java
+++ src/ca2110/searching.java
@@ -1,10 +1,13 @@
 package ca2110;
-
 public class searching<T> {
     public static void main(String[] args) {
-        Integer[] numbers = {10 , 20 , 90 , 100 , 120};
-     boolean result=searching.<Integer>linearSearch(numbers , 0,numbers.length-1 ,
-             80   );
+
+        Integer[] numbers = {10 , 20 , 90 , 100 , 120}; //Asc
+        Integer[] numbers2 = {120 , 100 , 90 , 20 , 10}; //Decs
+//     boolean result=searching.<Integer>linearSearch(numbers , 0,numbers.length-1 ,
+//             80   );
+
+     boolean result = searching.binarySearchDesc(numbers2,0, numbers2.length-1,10 );
 
      if(result)
          System.out.println("item found");
@@ -24,6 +27,38 @@ public class searching<T> {
             if(data[index].compareTo(target) > 0)
                 break;
             index++;
+        }
+        return found;
+    }
+
+    public static <T extends Comparable<T>> boolean binarySearch(T[] data , int min,int max , T target){
+        boolean found = false;
+        int low = min;
+        int high = max;
+        while(!found && low <= high){
+            int mid = (low + high) /2;
+            if(target.compareTo(data[mid]) == 0)
+                found = true;
+            else if (target.compareTo(data[mid]) > 0)
+                low  = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return found;
+    }
+
+    public static <T extends Comparable<T>> boolean binarySearchDesc(T[] data , int min,int max , T target){
+        boolean found = false;
+        int low = min;
+        int high = max;
+        while(!found && low <= high){
+            int mid = (low + high) /2;
+            if(target.compareTo(data[mid]) == 0)
+                found = true;
+            else if (target.compareTo(data[mid]) > 0)
+                high = mid - 1;
+            else
+                low  = mid + 1;
         }
         return found;
     }
